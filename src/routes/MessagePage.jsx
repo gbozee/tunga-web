0263a8da315c0136b7297deac2030ab9558d60dd
Abiola Oyeniyi <gbozee@gmail.com>
2017-12-13 10:09:08 +0100

    implemented mobile display of component.

--- src/routes/MessagePage.jsx
+++ src/routes/MessagePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled, {css} from 'styled-components'
 import {Link} from 'react-router';
 
 import SearchBox from '../components/SearchBox';
@@ -23,19 +24,42 @@ export function resizeOverviewBox() {
   }
 }
 
+const Div = styled.div`${props=>props.css}`
+
 class MessagePage extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      isShowing: false
+      isShowing: false,
+      isMobile: false,
+      isFetching: false,
+      list: { count: 0, ids: [] }
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    const { Channel: { list } } = nextProps
+    if (this.state.isFetching !== list.isFetching) {
+      this.setState({
+        isFetching: list.isFetching,
+        list
+      })
     }
   }
 
+  componentWillMount() {
+   
+    this.mediaQueryList = window.matchMedia("(max-width: 768px)")
+    this.mediaQueryList.addListener(this.updateMatches)
+    this.updateMatches()
+  }
+  componentWillUnmount() {
+    this.mediaQueryList.removeListener(this.updateMatches)
+  }
+  updateMatches = () => this.setState({ isMobile: this.mediaQueryList.matches })
   componentDidMount() {
     resizeOverviewBox();
     $(window).resize(resizeOverviewBox);
-
     this.getChannels();
   }
 
@@ -99,10 +123,13 @@ class MessagePage extends React.Component {
                 : 'Start a new conversation'}
             </a>
           </div>
-
+    const chatList = this.props.location.pathname === '/conversation/start'
+    const sideView = <SideBox isFetching={this.state.isFetching}
+      list={this.state.list} channel_type_filter={channel_type_filter} {...this.props}
+    />
     return (
-      <div id="chat-window">
-        <div className="col-lg-12 nopadding">
+      <div  id="chat-window">
+        <div  className="col-lg-12 nopadding">
 
           <div className="col-md-6 col-sm-6 col-xs-6  nopadding">
             {
@@ -123,69 +150,92 @@ class MessagePage extends React.Component {
           </div>
           <div className="col-xs-12 nopadding"><hr style={{marginTop: 0}}/></div>
         </div>
-        <div className="col-xs-12 nopadding">
-          <div className="chat-overview overview">
-            {isAuthenticated() &&
+        <div  className="col-xs-12 nopadding">
+          {this.state.isMobile && chatList ? sideView :
+          <Div css={css`
+          display: flex;
+          & .mainbox{
+            @media(max-width: 768px){
+              width: 100%;
+            }
+          }
+          `} className="chat-overview overview">
+            {isAuthenticated() && !this.state.isMobile &&
             (isAdmin() || channel_type_filter != CHANNEL_TYPES.support)
-              ? <div className="sidebox channelbox">
-                  <SearchBox
-                    placeholder="Search"
-                    onSearch={ChannelActions.listChannels}
-                    count={Channel.list.count}
-                    custom_class="search-box-group-custom"
-                  />
-                  {Channel.list.isFetching
-                    ? <Progress />
-                    : <div className="list-box">
-                        <div className="channel-list">
-                          {Channel.list.ids.map(id => {
-                            let channel = Channel.list.channels[id];
-                            return (
-                              <Link
-                                key={id}
-                                to={`${channel_type_filter ==
-                                CHANNEL_TYPES.support
-                                  ? '/help'
-                                  : '/conversation'}/${channel.id}/`}
-                                className="media"
-                                activeClassName="active">
-                                <div className="media-left">
-                                  <Avatar
-                                    src={
-                                      channel.user
-                                        ? channel.user.avatar_url
-                                        : null
-                                    }
-                                    icon={
-                                      channel.user ? null : 'glypichon-comment'
-                                    }
-                                    badge={channel.new || null}
-                                  />
-                                </div>
-                                <div className="media-body channel-details">
-                                  <ChannelInfo channel={channel} />
-                                </div>
-                              </Link>
-                            );
-                          })}
-                        </div>
-                        <LoadMore
-                          url={Channel.list.next}
-                          callback={ChannelActions.listMoreChannels}
-                          loading={Channel.list.isFetchingMore}
-                          text="more"
-                        />
-                      </div>}
-                </div>
-              : null}
+              ? sideView: null}
             <div className="mainbox">
               {this.renderChildren()}
             </div>
-          </div>
+        </Div>
+            }
         </div>
       </div>
     );
   }
 }
+const SideBoxStyles = styled.div`
+@media(max-width: 768px){
+  width: 100% !important;
+}
+`;
+class SideBox extends React.Component {
 
+  render() {
+
+    const { ChannelActions, channel_type_filter, list, isFetching } = this.props;
+    // debugger;
+    return (<SideBoxStyles className="sidebox channelbox">
+      <SearchBox
+        placeholder="Search"
+        onSearch={ChannelActions.listChannels}
+        count={list.count}
+        custom_class="search-box-group-custom"
+      />
+      {isFetching
+        ? <Progress />
+        : <div className="list-box">
+          <div className="channel-list">
+            {list.ids.map(id => {
+              let channel = list.channels[id];
+              return (
+                <Link
+                  key={id}
+                  to={`${channel_type_filter ==
+                    CHANNEL_TYPES.support
+                    ? '/help'
+                    : '/conversation'}/${channel.id}/`}
+                  className="media"
+                  activeClassName="active">
+                  <div className="media-left">
+                    <Avatar
+                      src={
+                        channel.user
+                          ? channel.user.avatar_url
+                          : null
+                      }
+                      icon={
+                        channel.user ? null : 'glypichon-comment'
+                      }
+                      badge={channel.new || null}
+                    />
+                  </div>
+                  <div className="media-body channel-details">
+                    <ChannelInfo channel={channel} />
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+          <LoadMore
+            url={list.next}
+            callback={ChannelActions.listMoreChannels}
+            loading={list.isFetchingMore}
+            text="more"
+          />
+        </div>}
+    </SideBoxStyles>
+
+    );
+  }
+}
 export default connect(MessagePage);
